b97cc1196ba1d56eaca579bab1e8411f475c6916
trinhmaingoc <trinhmn.dlhp@gmail.com>
2020-04-12 18:00:11 +0700

    Updates

---
+++ precache-manifest.8ddf1b5bd381f85bfeedcdc604e80aa7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db8658a19e2262e0c775dcd64581bd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c6a17b778d47ef84cbb",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f2b637610f27e4a4088",
+    "url": "/static/js/2.402e1242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.402e1242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6a17b778d47ef84cbb",
+    "url": "/static/js/main.780157cf.chunk.js"
+  },
+  {
+    "revision": "3c5629b0202501281187",
+    "url": "/static/js/runtime-main.be853bbd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
